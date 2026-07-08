054dfbec5c5fba81c4e3e3bca0c22fb81ade9675
jiubuntu <rlawldn0412@gmail.com>
2026-07-08 17:41:11 +0900

    chore: scaffold wms-server (Spring Boot) and wms-web (React+Vite) projects

---
+++ wms-server/src/test/java/com/jiubuntu/wms/WmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiubuntu.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
