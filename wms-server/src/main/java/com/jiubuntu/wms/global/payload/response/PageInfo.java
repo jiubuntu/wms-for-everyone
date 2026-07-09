d6c565afcaa5c2c39272e8c345103171e9ade376
jiubuntu <rlawldn0412@gmail.com>
2026-07-09 17:30:57 +0900

    feat(server): 공통 API 응답 wrapper 추가

--- wms-server/src/main/java/com/jiubuntu/wms/global/payload/response/PageInfo.java
+++ wms-server/src/main/java/com/jiubuntu/wms/global/payload/response/PageInfo.java
@@ -1,4 +1,20 @@
 package com.jiubuntu.wms.global.payload.response;
 
+import org.springframework.data.domain.Page;
+
 public class PageInfo {
+
+    private final int page;
+    private final int size;
+    private final long totalElements;
+    private final int totalPages;
+    private final boolean hasNext;
+
+    public PageInfo(Page page) {
+        this.page = page.getNumber() + 1;
+        this.size = page.getSize();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+        this.hasNext = page.hasNext();
+    }
 }
